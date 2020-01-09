79b410ba991dda9702fe7dfdbe34aee816fc0101
Briet Sparks <bws46@cornell.edu>
2020-01-08 22:15:36 -0600

    remove unused code

--- attributive-reducer/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { State } from './types';
-
-export const makeSelectors = <T>() => {
-  const getIds = (state: State<T>) => state.ids;
-  const getAllData = (state: State<T>) => state.data;
-  const getDataById = (state: State<T>, args: { id: string }) => state.data[args.id]
-};
